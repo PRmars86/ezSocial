f6ff50be5a0e5f0c32b56b302a15ef1164f339e1
PRmars86 <pengruima@gmail.com>
2023-06-03 12:55:52 -0700

    model

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production

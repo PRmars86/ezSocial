fe217de2c7709e3daf7d40db048ccc9a841751c6
PRmars86 <pengruima@gmail.com>
2023-06-03 21:10:12 -0700

    create group and post model

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,54 @@
+from django import template
+from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.text import slugify
+# from accounts.models import User
 
-# Create your models here.
+##import misaka
+import htmlgenerator as hg
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+register = template.Library()
+
+# https://docs.djangoproject.com/en/1.11/howto/custom-template-tags/#inclusion-tags
+# This is for the in_group_members check template tag
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(allow_unique=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = hg.HTML(
+            hg.HEAD(), hg.BODY(hg.H1(self.description)))
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group', 'user')
+
+    pass

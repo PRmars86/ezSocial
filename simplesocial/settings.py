cd88211bbf03c74a388b187116c2576541b9c231
PRmars86 <pengruima@gmail.com>
2023-06-03 20:06:59 -0700

    11

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "bootstrap3",
     'accounts',
 ]
 
@@ -114,6 +115,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = "test"
+LOGOUT_REDIRECT_URL = "thanks"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

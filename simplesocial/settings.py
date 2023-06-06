709857a67ef75d0d3c41bcb1f6198d0ee8dfa9ea
PRmars86 <pengruima@gmail.com>
2023-06-06 15:22:54 -0700

    1

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -27,8 +27,18 @@ SECRET_KEY = "django-insecure-8__i7qtziz6ey7x#y6)@(cr4_=@njs&=prch-ma1-6lf#qu0ek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+
+# SECURITY WARNING: don't run with debug turned on in production!
+if not IS_HEROKU_APP:
+    DEBUG = True
+
 ALLOWED_HOSTS = []
 
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -80,12 +90,26 @@ WSGI_APPLICATION = "simplesocial.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if IS_HEROKU_APP:
+    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
+    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
+    # automatically by Heroku when a database addon is attached to your Heroku app. See:
+    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
+    # https://github.com/jazzband/dj-database-url
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation

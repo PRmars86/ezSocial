3ff175804984cca9104e1c1cc143ed19ad4df47d
PRmars86 <pengruima@gmail.com>
2023-06-06 16:00:09 -0700

    11

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -139,9 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if not DEBUG else []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if DEBUG else []
 
 LOGIN_REDIRECT_URL = "test"
 LOGOUT_REDIRECT_URL = "thanks"

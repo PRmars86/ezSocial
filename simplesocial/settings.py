751c4315a78f1ccf3d557038a86d61c9bc0df22e
PRmars86 <pengruima@gmail.com>
2023-06-06 16:04:49 -0700

    11

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -139,8 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-if IS_HEROKU_APP:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if DEBUG else []
